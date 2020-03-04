5a5ed05c4eed58ffb8f7a570e37d041e8e876731
Meha Agrawal <agra.meha@hotmail.com>
2020-03-04 17:04:17 -0500

    checkout endpoing

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
 var db;
 
-MongoClient.connect(
-  "mongodb+srv://meha:dGbZhA9V4UeSJqHZ@cluster0-jgd6t.mongodb.net/test?retryWrites=true&w=majority",
-  (err, client) => {
-    if (err) return console.log(err);
-    db = client.db(“CRUD_Tutorial”);
-    app.listen(3000, function() {
-      console.log(“listening on 3000”);
-    });
-  }
-);
+MONGO_URL = "mongodb://127.0.0.1:27017";
+//"mongodb+srv://meha:0X4J02fFh1pQifzj@cluster0-tfnai.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(MONGO_URL, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db("volunteer_management");
+  app.listen(3000, function() {
+    console.log("listening on 3000");
+  });
+});
 
 /*
 
@@ -28,8 +27,8 @@ MongoClient.connect(
 */
 
 //Aakash
-app.get('/clockin', function (req, res) {
-    /*
+app.get("/clockin", function(req, res) {
+  /*
         Input:
             * checkin time: string
             * userid/name: int
@@ -37,12 +36,11 @@ app.get('/clockin', function (req, res) {
             * success: boolean
             * errorcode: int
      */
-
-})
+});
 
 //Meha
-app.get('/clockout', function(req, res) {
-    /*
+app.get("/clockout", function(req, res) {
+  /*
         Input:
             * checkout time: string
             * userid/name: int
@@ -53,42 +51,41 @@ app.get('/clockout', function(req, res) {
                 - "no current ongoing shift, clock out time added anyway"
      */
 
-      db.collection("users").find({ nfc_id: req.body.userid}, (response) => {
-        if (err) return console.log(err);
-        cur_shifts = response.shifts
+  db.collection("users").find({ nfc_id: req.body.userid }, response => {
+    if (err) return console.log(err);
+    cur_shifts = response.shifts;
 
-        if (cur_shifts != null) {
-          const cur_tuple  = cur_shifts[cur_shifts.length - 1]
-          cur_tuple.end_time = req.body.checkout_time;
-          db.vms.update(
-            {nfc_id: req.body.userid},
-            {$set:
-              {
-                shifts: cur_shifts
-              }
-            }
-          );
-        } else {
-          const cur_tuple = {"end_time": req.body.checkout_time};
-          const all_shifts = [cur_tuple];
-          db.vms.update(
-            {nfc_id: req.body.userid},
-            {$set:
-              {
-                shifts: all_shifts
-              }
-            }
-          );
-          console.log("no current ongoing shift, clock out time added anyway")
+    if (cur_shifts != null) {
+      const cur_tuple = cur_shifts[cur_shifts.length - 1];
+      cur_tuple.end_time = req.body.checkout_time;
+      db.vms.update(
+        { nfc_id: req.body.userid },
+        {
+          $set: {
+            shifts: cur_shifts
+          }
         }
-        res.send("Clock-out time added")
-
-      });
-})
+      );
+    } else {
+      const cur_tuple = { end_time: req.body.checkout_time };
+      const all_shifts = [cur_tuple];
+      db.vms.update(
+        { nfc_id: req.body.userid },
+        {
+          $set: {
+            shifts: all_shifts
+          }
+        }
+      );
+      console.log("no current ongoing shift, clock out time added anyway");
+    }
+    res.send("Clock-out time added");
+  });
+});
 
 //Rahul
-app.post('/edit-shift-history', function(req, res) {
-    /*
+app.post("/edit-shift-history", function(req, res) {
+  /*
         Input:
             * new_time
             * shift_number
@@ -97,13 +94,11 @@ app.post('/edit-shift-history', function(req, res) {
             * success: boolean
             * errorcode: int
      */
-})
+});
 
 //Rashmi
-app.post('/get-shift-history', function(req, res) {
-
-})
+app.post("/get-shift-history", function(req, res) {});
 
 app.listen(3000, function() {
-  console.log('listening on 3000')
-})
+  console.log("listening on 3000");
+});
