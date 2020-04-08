5198390f76b8a0dfa292f4fa63a6decaff83a7dc
Rashmi Athavale <rathavale6@gatech.edu>
2020-04-08 14:02:35 -0700

    added html table

--- client/volunteer-management/src/components/EditShiftHistory.js
+++ client/volunteer-management/src/components/EditShiftHistory.js
@@ -3,7 +3,46 @@ import React from 'react';
 const EditShiftHistory = () => {
   return(
     <div className="App">
-        <p>Edit shift history here</p>
+      <html>
+      <head>
+      <style>
+      table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+      th, td {
+        padding: 5px;
+        text-align: left;
+      }
+      </style>
+      </head>
+      <body>
+
+      <table style="width:100%">
+        <tr>
+          <th>Check In Time</th>
+          <th>Check Out Time</th>
+        </tr>
+        <tr>
+          <td>12:00pm</td>
+          <td>5:00pm</td>
+        </tr>
+        <tr>
+          <td>12:00pm</td>
+          <td>5:00pm</td>
+        </tr>
+        <tr>
+          <td>12:00pm</td>
+          <td>5:00pm</td>
+        </tr>
+        <tr>
+          <td>12:00pm</td>
+          <td>5:00pm</td>
+        </tr>
+      </table>
+
+      </body>
+      </html>
     </div>
   )
 }
