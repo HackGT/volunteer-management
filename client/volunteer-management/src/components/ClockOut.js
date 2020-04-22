451f737e27a38735bcd53f1326e40aeb5b715366
Rashmi Athavale <rathavale6@gatech.edu>
2020-04-22 14:31:30 -0700

    added api request

--- client/volunteer-management/src/components/ClockOut.js
+++ client/volunteer-management/src/components/ClockOut.js
@@ -1,9 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const clockoutErrorCodes = {
+  "1": "You have clocked out successfully",
+  "0": "You have clocked out, but forgot to clock in. Go to Edit Shift History to record your clock in time"
+};
 
 const ClockOut = ({ clockoutMessage }) => {
-  return(
-    <div className="App">
-        <p>{clockoutMessage}</p>
+  const [displayMessage, editDisplayMessage] = useState("");
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id: "1", clockout: "5:00" })
+    };
+    fetch('/clockout', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log("here")
+        console.log(data)
+        //{success: true, errorcode: 1}
+        var message = clockoutErrorCodes[data.errorcode]
+        editDisplayMessage(message)
+      });
+  })
+  return (
+    <div>
+      {displayMessage}
     </div>
   )
 }
--- client/volunteer-management/src/components/EditShiftHistory.js
+++ client/volunteer-management/src/components/EditShiftHistory.js
@@ -1,49 +1,50 @@
 import React from 'react';
 
 const EditShiftHistory = () => {
-  return(
-    <div className="App">
-      <html>
-      <head>
-      <style>
-      table, th, td {
-        border: 1px solid black;
-        border-collapse: collapse;
-      }
-      th, td {
-        padding: 5px;
-        text-align: left;
-      }
-      </style>
-      </head>
-      <body>
+  return (
+    ""
+    // <div className="App">
+    //   <html>
+    //   <head>
+    //   <style>
+    //   table, th, td {
+    //     border: 1px solid black;
+    //     border-collapse: collapse;
+    //   }
+    //   th, td {
+    //     padding: 5px;
+    //     text-align: left;
+    //   }
+    //   </style>
+    //   </head>
+    //   <body>
 
-      <table style="width:100%">
-        <tr>
-          <th>Check In Time</th>
-          <th>Check Out Time</th>
-        </tr>
-        <tr>
-          <td>12:00pm</td>
-          <td>5:00pm</td>
-        </tr>
-        <tr>
-          <td>12:00pm</td>
-          <td>5:00pm</td>
-        </tr>
-        <tr>
-          <td>12:00pm</td>
-          <td>5:00pm</td>
-        </tr>
-        <tr>
-          <td>12:00pm</td>
-          <td>5:00pm</td>
-        </tr>
-      </table>
+    //   <table style="width:100%">
+    //     <tr>
+    //       <th>Check In Time</th>
+    //       <th>Check Out Time</th>
+    //     </tr>
+    //     <tr>
+    //       <td>12:00pm</td>
+    //       <td>5:00pm</td>
+    //     </tr>
+    //     <tr>
+    //       <td>12:00pm</td>
+    //       <td>5:00pm</td>
+    //     </tr>
+    //     <tr>
+    //       <td>12:00pm</td>
+    //       <td>5:00pm</td>
+    //     </tr>
+    //     <tr>
+    //       <td>12:00pm</td>
+    //       <td>5:00pm</td>
+    //     </tr>
+    //   </table>
 
-      </body>
-      </html>
-    </div>
+    //   </body>
+    //   </html>
+    // </div>
   )
 }
 
