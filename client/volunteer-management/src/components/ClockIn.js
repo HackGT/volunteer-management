3464839f927538d606b01db42860184bde3069b5
agupte31 <aakashagupte@gatech.edu>
2020-04-22 18:06:21 -0400

    Backend Clockin

--- client/volunteer-management/src/components/ClockIn.js
+++ client/volunteer-management/src/components/ClockIn.js
@@ -1,12 +1,34 @@
-import React, { useState } from 'react'
+import React,{ useState, useEffect } from 'react';
+
+const errorCodes = {
+  "1": "You have clocked in successfully"
+  ,"0": "You have clocked in, however you previously forgot to clockout"
+}
 
 const ClockIn = () => {
-    return(
-        <div className="App">
-            <p>(Name) clocked in @ (time)</p>
-        </div>
+  const [message, editMessage] = useState([]);
 
-    )
+  useEffect(() => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: "1" , clockin: "1:00"})
+    };
+    fetch('/clockin', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log("here")
+            console.log(data)
+            var message = errorCodes[data.errorcode]
+            editMessage(message)
+        });
+  },[])
+  
+  return(
+    <div>
+        {message}
+    </div>
+  )
 }
 
 export default ClockIn;
